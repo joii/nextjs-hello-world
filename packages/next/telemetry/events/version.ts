ef5c0898ea4c6ca58a480c612c1d0e672d0c214d
Phatchara Yubon <contact.puipui@gmail.com>
2019-10-14 14:04:21 +0700

    from another_pc2

---
+++ packages/next/telemetry/events/version.ts
@@ -0,0 +1,27 @@
+const EVENT_VERSION = 'NEXT_CLI_SESSION_STARTED'
+
+type EventCliSessionStarted = {
+  nextVersion: string
+  nodeVersion: string
+  cliCommand: string
+}
+
+export function eventVersion(
+  event: Omit<EventCliSessionStarted, 'nextVersion' | 'nodeVersion'>
+): { eventName: string; payload: EventCliSessionStarted }[] {
+  // This should be an invariant, if it fails our build tooling is broken.
+  if (typeof process.env.__NEXT_VERSION !== 'string') {
+    return []
+  }
+
+  return [
+    {
+      eventName: EVENT_VERSION,
+      payload: {
+        nextVersion: process.env.__NEXT_VERSION,
+        nodeVersion: process.version,
+        cliCommand: event.cliCommand,
+      } as EventCliSessionStarted,
+    },
+  ]
+}
